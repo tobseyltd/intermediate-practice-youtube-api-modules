14419569be80937a4b41bce43e14c74f664145d8
tobseyltd <mail@tobsey.limited>
2023-02-23 07:57:42 +0100

    commit #1

---
+++ js/modules/youtube.js
@@ -0,0 +1,41 @@
+export const get_youtube_Genres = async (APIKEY) => {
+
+    try {
+        const RESPONSE = await fetch(`https://www.googleapis.com/youtube/v3/videoCategories/?regionCode=de&key=${APIKEY}`, {
+            method: 'GET'
+        });
+
+        if (RESPONSE.ok) {
+            const jsonResponse = await RESPONSE.json();
+            return jsonResponse.items;
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+};
+
+export const get_vidz_By_cat = async (APIKEY, CATEGORY) => {
+    
+    // const CATEGORY = get_selected_Genre();
+
+    try {
+        const RESPONSE = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=id&chart=mostPopular&regionCode=de&videoCategoryId=${CATEGORY}&key=${APIKEY}`, {
+            method: 'GET'
+        });
+
+        if (RESPONSE.ok) {
+            const jsonResponse = await RESPONSE.json();
+            return jsonResponse.items;
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+
+export const get_selected_Genre = (domElement) => {
+
+    category = domElement.value;
+    return parseInt(category);
+};
